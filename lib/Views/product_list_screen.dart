4d479be192a5ee59a11195124c74ac7d602831d8
HamzaIqbalMughal <humza.iqbal.hi@gmail.com>
2023-11-22 16:56:26 +0500

    added productListScreen

---
+++ lib/Views/product_list_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+
+class ProductListScreen extends StatefulWidget {
+  const ProductListScreen({super.key});
+
+  static const String screen_id = "ProductListScreen";
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Product List',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          badges.Badge(
+            badgeContent: Text('0'),
+            badgeAnimation: ,
+          ),
+          Icon(Icons.shopping_bag_outlined, color: Colors.white,),
+          SizedBox(width: 20,),
+        ],
+        backgroundColor: Colors.lightBlue,
+      ),
+    );
+  }
+}
