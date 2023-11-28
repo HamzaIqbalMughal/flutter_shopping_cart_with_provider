7c691ccde64d574deb3f929f64dca64a603d90b2
HamzaIqbalMughal <humza.iqbal.hi@gmail.com>
2023-11-29 00:21:15 +0500

    for lab

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
           title: 'Flutter Demo',
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
-              // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              // useMaterial3: true,
-              ),
+            primarySwatch: Colors.blue,
+            useMaterial3: true,
+          ),
           home: ProductListScreen(),
         );
       }),
